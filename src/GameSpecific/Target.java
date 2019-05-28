9d0e76aa7e0ecb970456348024ff717e6d368646
Amartya Bhattacharya <amartyacharya@gmail.com>
2019-05-27 20:32:43 -0700

    More work on Target

--- src/GameSpecific/Target.java
+++ src/GameSpecific/Target.java
@@ -5,8 +5,12 @@ import Structure.ActorBox;
 public class Target extends ActorBox {
     private boolean isHit;
 
-    public boolean isHit() {
+    boolean isHit() {
         return isHit;
+    }
+
+    void setHit(boolean hit) {
+        isHit = hit;
     }
 
     public Target(double width, double height, double depth) {
